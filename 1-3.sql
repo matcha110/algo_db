e3d7886df9a1501d3d6c6192c4ff17ceb234761d
matcha110 <121884964+matcha110@users.noreply.github.com>
2024-06-13 16:44:53 +0900

    Update 1-3.sql

--- 1-3.sql
+++ 1-3.sql
@@ -1,8 +1,1 @@
-SELECT * FROM prefectures ORDER BY area DESC LIMIT 10;
-
-SELECT * FROM prefectures WHERE name LIKE '%島%';
-
-SELECT
-    MAX(highest) AS '最高気温',
-    MIN(lowest) AS '最低気温'
-FROM temperature_august;
+SELECT * FROM prefectures WHERE population < 1000000;
