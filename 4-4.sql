910460692c4dab157b3386ee1138f150ad8fbdf5
matcha110 <121884964+matcha110@users.noreply.github.com>
2024-08-06 17:50:54 +0900

    Update 4-4.sql

--- 4-4.sql
+++ 4-4.sql
@@ -1,8 +1,15 @@
-SELECT * FROM prefectures ORDER BY area DESC LIMIT 10;
-
-SELECT * FROM prefectures WHERE name LIKE '%島%';
-
+-- WITH句：副問い合わせ
+WITH ranked_results AS (
+    SELECT
+        ROW_NUMBER() OVER(ORDER BY score DESC, id) '順位',
+        name AS '名前',
+        score AS 'スコア'
+    FROM
+        results
+)
 SELECT
-    MAX(highest) AS '最高気温',
-    MIN(lowest) AS '最低気温'
-FROM temperature_august;
+    *
+FROM
+    ranked_results
+WHERE
+    `順位` IN (1, 2, 3, 10);
