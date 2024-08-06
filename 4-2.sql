bfd8a1351b18c83a04f29b8cb165c03ec6da4dd1
matcha110 <121884964+matcha110@users.noreply.github.com>
2024-08-06 17:46:25 +0900

    Update 4-2.sql

--- 4-2.sql
+++ 4-2.sql
@@ -1,8 +1,12 @@
-SELECT * FROM prefectures ORDER BY area DESC LIMIT 10;
-
-SELECT * FROM prefectures WHERE name LIKE '%島%';
-
 SELECT
-    MAX(highest) AS '最高気温',
-    MIN(lowest) AS '最低気温'
-FROM temperature_august;
+    subject AS '科目',
+    COUNT(score BETWEEN 90 AND 100 OR NULL) AS '秀',
+    COUNT(score BETWEEN 80 AND 89 OR NULL) AS '優',
+    COUNT(score BETWEEN 65 AND 79 OR NULL) AS '良',
+    COUNT(score BETWEEN 50 AND 64 OR NULL) AS '可',
+    COUNT(score BETWEEN 0 AND 49 OR NULL) AS '不可'
+FROM
+    grades
+GROUP BY
+    subject;
+    
